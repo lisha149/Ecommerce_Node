b726529721e7165794d5309d213b704ffdb36e4c
Palisha Shakya <palishashakya@gmail.com>
2022-11-30 21:47:05 +0545

    get all the products in json format

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const fs = require("fs");
+
 const app = express();
 app.use(express.json());
 
-//endpoint to validate sever setup
+// endpoint to validate sever setup
 app.get("/", (req, res) => {
   res.send("API is working....");
+});
+
+//endpoint to get all the products
+app.get("/api/products", (req, res) => {
+  fs.readFile(__dirname + "/" + "products.json", "utf8", (err, products) => {
+    if (err) {
+      res.status(404).json({ message: err });
+    }
+    console.log(products);
+    res.status(200).send(products);
+  });
 });
 
 const PORT = 5000;
