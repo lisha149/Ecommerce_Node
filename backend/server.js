3d5c52358f14807ac2e2daf0081bbb509e25ec63
Palisha Shakya <palishashakya@gmail.com>
2022-12-01 17:23:14 +0545

    json-server removed and write data using fs

--- backend/server.js
+++ backend/server.js
@@ -16,16 +16,36 @@ app.get("/", (req, res) => {
 });
 
 //endpoint to get all the products
-//using json-server to access and modify resources,
-// app.get("/api/products", (req, res) => {
-//   fs.readFile(__dirname + "/" + "products.json", "utf8", (err, products) => {
-//     if (err) {
-//       res.status(404).json({ message: err });
-//     }
-//     // console.log(products);
-//     res.status(200).send(products);
-//   });
-// });
+app.get("/api/products", (req, res) => {
+  fs.readFile(__dirname + "/" + "products.json", "utf8", (err, products) => {
+    if (err) {
+      res.status(404).json({ message: err });
+    }
+    // console.log(products);
+    res.status(200).send(products);
+  });
+});
+
+//endpoint to add the products
+app.post("/api/products/create", (req, res) => {
+  const data = fs.readFileSync(__dirname + "/" + "products.json", "utf8");
+  let products = JSON.parse(data);
+  // console.log(products);
+  const newProduct = req.body;
+  products.push(newProduct);
+  res.json(products);
+  fs.writeFile(
+    __dirname + "/" + "products.json",
+    JSON.stringify(products, null, 2),
+    (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Products Added Successfully");
+      }
+    }
+  );
+});
 
 const PORT = 5000;
 app.listen(PORT, () =>
